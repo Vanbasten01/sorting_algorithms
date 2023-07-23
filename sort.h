3c8471ff8c66bf9303ae73370020e2259eb3d4f9
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-23 20:06:31 +0100

    Cocktail Sort

--- sort.h
+++ sort.h
@@ -21,11 +21,12 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
-void swap_list(listint_t *nodea, listint_t *nodeb);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void quicksort(int *array, int low, int high, size_t size);
 unsigned int _lomuto(int *arr, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **head, listint_t *a, listint_t *b);
 #endif
