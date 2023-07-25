234a015862354f4ba911dbe6244f71e36b588878
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-26 00:00:03 +0100

    Counting Sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 /**
- *
- *
- *
- *
+ *counting_sort - a function that sorts an array using
+ *the counting sort algorithm
+ *@array: the array to be sorted.
+ *@size: the size of the array.
+ *Return: void.
  */
 void counting_sort(int *array, size_t size)
 {
