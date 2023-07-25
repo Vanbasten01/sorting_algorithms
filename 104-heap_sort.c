b4cd1175cd5e8c5e337852db8cec94b5420939c2
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-26 00:20:12 +0100

    Heap Sort (betty style)

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -14,10 +14,11 @@ void swap(int *a, int *b)
 	*b = c;
 }
 /**
- *
- *
- *
- *
+ *heap_sort - afuntion that sorts an array of integers in ascending
+ *order using Heap Sort algorithm.
+ *@array: a pointer to the array to be sorted.
+ *@size: the size of the array.
+ *Return: void.
  */
 void heap_sort(int *array, size_t size)
 {
@@ -28,22 +29,25 @@ void heap_sort(int *array, size_t size)
 		return;
 
 	for (i = (int)size / 2 - 1; i >= 0; i--)
-		heapfy(array, size, i, array_size);
+		heapify(array, size, i, array_size);
 
 	for (i = size - 1; i >= 1; i--)
 	{
 		swap(&array[i], &array[0]);
 		print_array(array, array_size);
-		heapfy(array, i, 0, array_size);
+		heapify(array, i, 0, array_size);
 	}
 }
 /**
- *
- *
- *
- *
+ *heapify - a function that rearranges a binary heap
+ *to maintain the max heap property.
+ *@array: a pointer to the array to be sorted.
+ *@size: the size of the current heap.
+ *@i: index of the current node to be heapefied.
+ *@array_size: the total size of the original array.
+ *Return: void.
  */
-void heapfy(int *array, size_t size, int i, size_t array_size)
+void heapify(int *array, size_t size, int i, size_t array_size)
 {
 	int largest = i, left = 2 * i + 1, right = 2 * i + 2;
 
@@ -55,7 +59,6 @@ void heapfy(int *array, size_t size, int i, size_t array_size)
 	{
 		swap(&array[i], &array[largest]);
 		print_array(array, array_size);
-		heapfy(array, size, largest, array_size);
+		heapify(array, size, largest, array_size);
 	}
-
 }
